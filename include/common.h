35b319b4a39c64f26eddcf35b2fb8cebdc5f5e08
cp_shen <cp_shen@outlook.com>
2020-05-02 23:20:26 +0800

    code cleanup

--- include/common.h
+++ include/common.h
@@ -4,10 +4,14 @@
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
 
-#include <learnopengl/camera.h>
+#include "learnopengl/camera.h"
+#include <GLFW/glfw3.h>
+#include <TextEditor.h>
 
 void free_render_resources();
 void feed_render_resources();
+void editor_setup(TextEditor &);
+void process_window_main_loop(GLFWwindow *);
 GLuint do_offscreen_rendering(Camera &camera);
 unsigned int loadTexture(const char *path);
 void read_pixels_and_save(int w, int h, const char *path);
--- src/main.cpp
+++ src/main.cpp
@@ -25,16 +25,12 @@
 using namespace std;
 
 static Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-static float lastX = (float)SCR_WIDTH / 2.0;
-static float lastY = (float)SCR_HEIGHT / 2.0;
 static TextEditor editor;
 static GLuint texId;
 
 static void error_callback(int error, const char *description) {
     fprintf(stderr, "Error %d: %s\n", error, description);
 }
-
-void editor_setup(TextEditor &);
 
 int main(int, char **) {
     // Setup window
@@ -148,221 +144,12 @@ int main(int, char **) {
     // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
     // NULL, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
 
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     editor_setup(editor);
-
     feed_render_resources();
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
-        // tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data
-        // to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
-        // data to your main application. Generally you may always pass all
-        // inputs to dear imgui, and hide them from your application based on
-        // those two flags.
-        glfwPollEvents();
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // 1. Show the big demo window (Most of the sample code is in
-        // ImGui::ShowDemoWindow()! You can browse its code to learn more about
-        // Dear ImGui!).
-        ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
-        ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver);
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End
-        // pair to created a named window.
-        ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
-        ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver);
-        ImGui::Begin("Hello, world!");
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Text(
-                "This is some useful text."); // Display some text (you can use
-                                              // a format strings too)
-            ImGui::Checkbox("Demo Window",
-                            &show_demo_window); // Edit bools storing our window
-                                                // open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat(
-                "float", &f, 0.0f,
-                1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3(
-                "clear color",
-                (float *)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button(
-                    "Button")) // Buttons return true when clicked (most widgets
-                               // return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                        1000.0f / ImGui::GetIO().Framerate,
-                        ImGui::GetIO().Framerate);
-        }
-        ImGui::End();
-
-        // 3. Show another simple window.
-        if (show_another_window) {
-            ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
-            ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver);
-            ImGui::Begin(
-                "Another Window",
-                &show_another_window); // Pass a pointer to our bool variable
-                                       // (the window will have a closing button
-                                       // that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
-        ImGui::SetNextWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
-        ImGui::Begin("Game rendering");
-        {
-            // OpenGL offscreen rendering
-            process_camara_input(camera);
-            texId = do_offscreen_rendering(camera);
-
-            // Get the current cursor position (where your window is)
-            ImVec2 pos = ImGui::GetCursorScreenPos();
-
-            // ImGui::Image((void *)texId, ImVec2(SCR_WIDTH, SCR_HEIGHT),
-            //              ImVec2(0, 0), ImVec2(1, 1));
-
-            ImGui::GetWindowDrawList()->AddImage(
-                (void *)texId, pos,
-                ImVec2(pos.x + SCR_WIDTH, pos.y + SCR_HEIGHT), ImVec2(0, 1),
-                ImVec2(1, 0));
-        }
-        ImGui::End();
-
-        ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
-        ImGui::SetNextWindowPos(ImVec2(800, 600), ImGuiCond_FirstUseEver);
-        ImGui::Begin("Text Editor Demo", nullptr,
-                     ImGuiWindowFlags_HorizontalScrollbar |
-                         ImGuiWindowFlags_MenuBar);
-        {
-            auto cpos = editor.GetCursorPosition();
-            if (ImGui::BeginMenuBar()) {
-                if (ImGui::BeginMenu("File")) {
-                    if (ImGui::MenuItem("Save")) {
-                        auto textToSave = editor.GetText();
-                        /// save text....
-                    }
-                    if (ImGui::MenuItem("Quit", "Alt-F4"))
-                        break;
-                    ImGui::EndMenu();
-                }
-                if (ImGui::BeginMenu("Edit")) {
-                    bool ro = editor.IsReadOnly();
-                    if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
-                        editor.SetReadOnly(ro);
-                    ImGui::Separator();
-
-                    if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr,
-                                        !ro && editor.CanUndo()))
-                        editor.Undo();
-                    if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr,
-                                        !ro && editor.CanRedo()))
-                        editor.Redo();
-
-                    ImGui::Separator();
-
-                    if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr,
-                                        editor.HasSelection()))
-                        editor.Copy();
-                    if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr,
-                                        !ro && editor.HasSelection()))
-                        editor.Cut();
-                    if (ImGui::MenuItem("Delete", "Del", nullptr,
-                                        !ro && editor.HasSelection()))
-                        editor.Delete();
-                    if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr,
-                                        !ro && ImGui::GetClipboardText() !=
-                                                   nullptr))
-                        editor.Paste();
-
-                    ImGui::Separator();
-
-                    if (ImGui::MenuItem("Select all", nullptr, nullptr))
-                        editor.SetSelection(
-                            TextEditor::Coordinates(),
-                            TextEditor::Coordinates(editor.GetTotalLines(), 0));
-
-                    ImGui::EndMenu();
-                }
-
-                if (ImGui::BeginMenu("View")) {
-                    if (ImGui::MenuItem("Dark palette"))
-                        editor.SetPalette(TextEditor::GetDarkPalette());
-                    if (ImGui::MenuItem("Light palette"))
-                        editor.SetPalette(TextEditor::GetLightPalette());
-                    if (ImGui::MenuItem("Retro blue palette"))
-                        editor.SetPalette(TextEditor::GetRetroBluePalette());
-                    ImGui::EndMenu();
-                }
-                ImGui::EndMenuBar();
-            }
-
-            ImGui::Text(
-                "%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1,
-                cpos.mColumn + 1, editor.GetTotalLines(),
-                editor.IsOverwrite() ? "Ovr" : "Ins",
-                editor.CanUndo() ? "*" : " ",
-                editor.GetLanguageDefinition().mName.c_str(), "filename here");
-
-            editor.Render("TextEditor");
-        }
-        ImGui::End();
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z,
-                     clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        // Update and Render additional Platform Windows
-        // (Platform functions may change the current OpenGL context, so we
-        // save/restore it to make it easier to paste this code elsewhere.
-        //  For this specific demo app we could also call
-        //  glfwMakeContextCurrent(window) directly)
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-            GLFWwindow *backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-            glfwMakeContextCurrent(backup_current_context);
-        }
-
-        glfwSwapBuffers(window);
-
-        static bool saved = false;
-        if (!saved) {
-            save_texture(SCR_WIDTH, SCR_HEIGHT, texId, "out.png");
-            saved = true;
-        }
+        process_window_main_loop(window);
     }
 
     free_render_resources();
@@ -376,4 +163,225 @@ int main(int, char **) {
     glfwTerminate();
 
     return 0;
+}
+
+void process_window_main_loop(GLFWwindow *window) {
+    IM_ASSERT(
+        ImGui::GetCurrentContext() != NULL &&
+        "Missing dear imgui context. Refer to examples app!"); // Exceptionally
+                                                               // add an extra
+                                                               // assert here
+                                                               // for people
+                                                               // confused with
+                                                               // initial dear
+                                                               // imgui setup
+
+    // Our state
+    static bool show_demo_window = true;
+    static bool show_another_window = false;
+    static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    ImGuiIO &io = ImGui::GetIO();
+
+    // Poll and handle events (inputs, window resize, etc.)
+    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
+    // tell if dear imgui wants to use your inputs.
+    // - When io.WantCaptureMouse is true, do not dispatch mouse input data
+    // to your main application.
+    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
+    // data to your main application. Generally you may always pass all
+    // inputs to dear imgui, and hide them from your application based on
+    // those two flags.
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // 1. Show the big demo window (Most of the sample code is in
+    // ImGui::ShowDemoWindow()! You can browse its code to learn more about
+    // Dear ImGui!).
+    ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver);
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+    // 2. Show a simple window that we create ourselves. We use a Begin/End
+    // pair to created a named window.
+    ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver);
+    ImGui::Begin("Hello, world!");
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Text("This is some useful text."); // Display some text (you can
+                                                  // use a format strings too)
+        ImGui::Checkbox("Demo Window",
+                        &show_demo_window); // Edit bools storing our window
+                                            // open/close state
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat(
+            "float", &f, 0.0f,
+            1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3(
+            "clear color",
+            (float *)&clear_color); // Edit 3 floats representing a color
+
+        if (ImGui::Button(
+                "Button")) // Buttons return true when clicked (most widgets
+                           // return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                    1000.0f / ImGui::GetIO().Framerate,
+                    ImGui::GetIO().Framerate);
+    }
+    ImGui::End();
+
+    // 3. Show another simple window.
+    if (show_another_window) {
+        ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
+        ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver);
+        ImGui::Begin(
+            "Another Window",
+            &show_another_window); // Pass a pointer to our bool variable
+                                   // (the window will have a closing button
+                                   // that will clear the bool when clicked)
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me"))
+            show_another_window = false;
+        ImGui::End();
+    }
+
+    ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+    ImGui::Begin("Game rendering");
+    {
+        // OpenGL offscreen rendering
+        process_camara_input(camera);
+        texId = do_offscreen_rendering(camera);
+
+        // Get the current cursor position (where your window is)
+        ImVec2 pos = ImGui::GetCursorScreenPos();
+
+        // ImGui::Image((void *)texId, ImVec2(SCR_WIDTH, SCR_HEIGHT),
+        //              ImVec2(0, 0), ImVec2(1, 1));
+
+        ImGui::GetWindowDrawList()->AddImage(
+            (void *)texId, pos, ImVec2(pos.x + SCR_WIDTH, pos.y + SCR_HEIGHT),
+            ImVec2(0, 1), ImVec2(1, 0));
+    }
+    ImGui::End();
+
+    ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowPos(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+    ImGui::Begin("Text Editor Demo", nullptr,
+                 ImGuiWindowFlags_HorizontalScrollbar |
+                     ImGuiWindowFlags_MenuBar);
+    {
+        auto cpos = editor.GetCursorPosition();
+        if (ImGui::BeginMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("Save")) {
+                    auto textToSave = editor.GetText();
+                    /// save text....
+                }
+                if (ImGui::MenuItem("Quit", "Alt-F4"))
+                    glfwSetWindowShouldClose(window, 1);
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Edit")) {
+                bool ro = editor.IsReadOnly();
+                if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
+                    editor.SetReadOnly(ro);
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr,
+                                    !ro && editor.CanUndo()))
+                    editor.Undo();
+                if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr,
+                                    !ro && editor.CanRedo()))
+                    editor.Redo();
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr,
+                                    editor.HasSelection()))
+                    editor.Copy();
+                if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr,
+                                    !ro && editor.HasSelection()))
+                    editor.Cut();
+                if (ImGui::MenuItem("Delete", "Del", nullptr,
+                                    !ro && editor.HasSelection()))
+                    editor.Delete();
+                if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr,
+                                    !ro &&
+                                        ImGui::GetClipboardText() != nullptr))
+                    editor.Paste();
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Select all", nullptr, nullptr))
+                    editor.SetSelection(
+                        TextEditor::Coordinates(),
+                        TextEditor::Coordinates(editor.GetTotalLines(), 0));
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("View")) {
+                if (ImGui::MenuItem("Dark palette"))
+                    editor.SetPalette(TextEditor::GetDarkPalette());
+                if (ImGui::MenuItem("Light palette"))
+                    editor.SetPalette(TextEditor::GetLightPalette());
+                if (ImGui::MenuItem("Retro blue palette"))
+                    editor.SetPalette(TextEditor::GetRetroBluePalette());
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+
+        ImGui::Text(
+            "%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1,
+            cpos.mColumn + 1, editor.GetTotalLines(),
+            editor.IsOverwrite() ? "Ovr" : "Ins", editor.CanUndo() ? "*" : " ",
+            editor.GetLanguageDefinition().mName.c_str(), "filename here");
+
+        editor.Render("TextEditor");
+    }
+    ImGui::End();
+
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    // Update and Render additional Platform Windows
+    // (Platform functions may change the current OpenGL context, so we
+    // save/restore it to make it easier to paste this code elsewhere.
+    //  For this specific demo app we could also call
+    //  glfwMakeContextCurrent(window) directly)
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+        GLFWwindow *backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+    }
+
+    glfwSwapBuffers(window);
+
+    static bool saved = false;
+    if (!saved) {
+        save_texture(SCR_WIDTH, SCR_HEIGHT, texId, "out.png");
+        saved = true;
+    }
 }
